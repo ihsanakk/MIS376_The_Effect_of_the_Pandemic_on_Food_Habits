059e45065ead490dd863ec10937f25a89435c963
ihsanakk <67482437+ihsanakk@users.noreply.github.com>
2021-05-26 11:07:37 +0300

    Add files via upload

---
+++ datasets/datamapping.js
@@ -0,0 +1,47 @@
+const beforeCovidMealNumbers  = [
+    {two_meal:29.8},
+    {three_meal:65.8},
+    {four_meal:4.3},
+    {fiveOrMore_meal:0.3}
+];
+
+
+const covidPeriodMealNumbers = [
+
+    {two_meal:33.8},
+    {three_meal:49.8},
+    {four_meal:13.5},
+    {fiveOrMore_meal:3.0}
+
+];
+
+
+const eatingHabitsChange = [
+
+    {YES:45.3},
+    {SOMETIMES:22.8},
+    {NO:32.0}
+
+];
+
+
+const budgetAffect = [
+
+    {YES:43.0},
+    {SOMETIMES:27.8},
+    {NO:29.3}
+
+];
+
+
+const foodTypes = [
+
+    {Pastry:28.0},
+    {MeatProducts:18.5},
+    {VegetableTypes:15.0},
+    {Desserts:9.5},
+    {Seasonings:1.0},
+    {Snacks:13.0},
+    {Fruits:15.0}
+
+];
